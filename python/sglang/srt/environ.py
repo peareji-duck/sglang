7e01a1ac3558f4b4aef09b2adc3664f81584d1ce
peareji-duck <295055959+peareji-duck@users.noreply.github.com>
2026-06-29 09:25:18 +0900

    perf: add dLLM consumer-sufficient TP vocab state
    Signed-off-by: peareji-duck <minssub.kim@navercorp.com>

--- python/sglang/srt/environ.py
+++ python/sglang/srt/environ.py
@@ -231,6 +231,8 @@ class Envs:
     SGLANG_DISABLED_MODEL_ARCHS = EnvTuple(tuple())
     SGLANG_PREFETCH_BLOCK_SIZE_MB = EnvInt(16)
     SGLANG_GEMMA_OUT_OF_PLACE_POSITION_MUTATION = EnvBool(False)
+    SGLANG_DLLM_TP_LOCAL_VOCAB = EnvBool(False)
+    SGLANG_DLLM_TP_LOCAL_VOCAB_PACKED_GATHER = EnvBool(True)
 
     # Logging Options
     SGLANG_LOG_GC = EnvBool(False)
